b9aa14aa5d33690eb806c2de54e19a9445c75fc4
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:12:15 -0500

    he creado la clase vuelaconalas

---
+++ src/pt_programa001/VuelaConAlas.java
@@ -0,0 +1,12 @@
+
+package pt_programa001;
+
+
+public class VuelaConAlas implements Volable{
+
+    @Override
+    public void volar() {
+        System.out.println("Puedo volar con mis alas");
+    }
+    
+}
