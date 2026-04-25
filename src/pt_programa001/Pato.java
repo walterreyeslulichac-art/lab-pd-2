6003c44c4c09402bd2a19d513d816d395c7be61c
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:33:59 -0500

    cambie la clase abtsracta pato e implemente las interfaces volable y cuaqueable y añadi los metodos realizarvuelo y realizarcuaqueo

--- src/pt_programa001/Pato.java
+++ src/pt_programa001/Pato.java
@@ -1,9 +1,28 @@
 
 package pt_programa001;
 
-public abstract class Pato {
+public abstract class Pato{
+    protected Volable volable;
+    protected Cuaqueable cuaqueable;
+    
+    
+    public void setVolable(Volable v) {
+        this.volable = v;
+    }
+
+    public void setCuaqueable(Cuaqueable c) {
+        this.cuaqueable = c;
+    }
+    
+    public abstract void  mostrar();
+    public void RealizarVuelo(){
+        volable.volar();
+    }
+    public void RealizarCuaqueo(){
+        cuaqueable.cuaquear();
+    }
+    
     public void nadar(){
         System.out.println("TODOS LOS PATOS NADAN");
     }
-    public abstract void mostrar();
 }
