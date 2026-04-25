94a2d93b71effc1932aab74e1b6d6e1909e38b9b
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:38:10 -0500

    hice cambios en la clase cabeza roja para poder implementar solo la clase pato y quite su conexion con las interfaces

--- src/pt_programa001/PatoCabezaRoja.java
+++ src/pt_programa001/PatoCabezaRoja.java
@@ -8,20 +8,18 @@ package pt_programa001;
  *
  * @author sistemas
  */
-public class PatoCabezaRoja extends Pato implements Volable,Cuaqueable{
+public class PatoCabezaRoja extends Pato {
 
+    public PatoCabezaRoja(){
+        volable=new VuelaConAlas();
+        cuaqueable=new Cuaqueo();
+    }
     @Override
     public void mostrar() {
         System.out.println("Yo soy un real PATO CABEZA ROJA");
     }
 
-    @Override
-    public void volar() {
-        System.out.println("Pudo volar con mis alas");
-    }
 
-    @Override
-    public void cuaquear() {
-        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");    }
+       
     
 }
