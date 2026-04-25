3604b430424b66ec4146d4933e33e23269f53e67
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:04:29 -0500

    he creado la clase cuaqueo

---
+++ src/pt_programa001/Cuaqueo.java
@@ -0,0 +1,12 @@
+
+package pt_programa001;
+
+
+public class Cuaqueo implements Cuaqueable{
+
+    @Override
+    public void cuaquear() {
+        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");
+    }
+    
+}
