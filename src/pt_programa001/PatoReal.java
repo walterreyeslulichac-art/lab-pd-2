42cea0761ebdbfdfb3c513c239eeccd722189d75
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:42:04 -0500

    he cambiado la clase pato real

--- src/pt_programa001/PatoReal.java
+++ src/pt_programa001/PatoReal.java
@@ -8,21 +8,16 @@ package pt_programa001;
  *
  * @author Reyes Lulichac Walter Adrian
  */
-public class PatoReal extends Pato implements Volable,Cuaqueable {
-
+public class PatoReal extends Pato {
+    public PatoReal(){
+        volable=new VuelaConAlas();
+        cuaqueable=new Cuaqueo();
+    }
     @Override
     public void mostrar() {
         System.out.println("Yo soy un verdadero PATO REAL");
     }
 
-    @Override
-    public void volar() {
-        System.out.println("Puedo volar con mis alas");
-    }
-
-    @Override
-    public void cuaquear() {
-        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");
-    }
+    
     
 }
