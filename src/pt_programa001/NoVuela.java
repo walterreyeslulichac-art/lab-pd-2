c4d32c364dd78cf99d9338b462616d846ef43e48
Walter Reyes <walterreyeslulichac@gmail.com>
2026-04-25 11:14:17 -0500

    he creado la clase incapazdevolar

---
+++ src/pt_programa001/NoVuela.java
@@ -0,0 +1,12 @@
+
+package pt_programa001;
+
+
+public class NoVuela implements Volable{
+
+    @Override
+    public void volar() {
+        System.out.println("INCAPAZ DE VOLAR");   
+    }
+    
+}
